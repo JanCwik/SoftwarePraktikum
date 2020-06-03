502ffa1538d37483ea9671f5fb0cf43e00f6e3fb
SvenjaHolzinger <sh2929@hdm-stuttgart.de>
2020-06-03 17:29:55 +0200

    print

--- BusinessLogic/BusinessObject.py
+++ BusinessLogic/BusinessObject.py
@@ -5,10 +5,10 @@ class BusinessObject(ABC):
     def __init__(self):
         self._id = 0   # Die eindeutige Identifikationsnummer einer Instanz dieser Klasse.
 
-    def get_id(self):
-        """Auslesen der ID."""
-        return self._id
-
     def set_id(self,id):
         """Setzen der ID."""
         self._id = id
+
+    def get_id(self):
+        """Auslesen der ID."""
+        return self._id
