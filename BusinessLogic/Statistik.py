26200c41350aeacefb17bd255678f11b021cdb72
SvenjaHolzinger <sh2929@hdm-stuttgart.de>
2020-06-03 18:23:09 +0200

    Statistik

---
+++ BusinessLogic/Statistik.py
@@ -0,0 +1,9 @@
+class Statistik():
+    def __init__(self):
+        pass
+
+    def filter_zeitraum(self, anfangszeitpunkt, endzeitpunkt):
+        pass
+
+    def filter_einzelhändler(self, Einzelhändler):
+        pass
