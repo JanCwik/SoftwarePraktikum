587671bfa03126aa73b58e5cccde2f8d22adfcdb
SvenjaHolzinger <sh292@hdm-stuttgart.de>
2020-06-03 18:16:11 +0200

    ApplikationsAdministration

--- BusinessLogic/ApplikationsAdministration.py
+++ BusinessLogic/ApplikationsAdministration.py
@@ -6,22 +6,8 @@ class ApplikationsAdministration(object):
     def __init__(self):
         pass
 
-    def artikel_anlegen(self,name,einheit,standardartikel):
+    def artikel_anlegen(self, name, einheit, standardartikel):
         artikel = Artikel()
-        Artikel.NamedBO.set_name(name)
+        artikel.NamedBO.set_name(name)
         artikel.set_einheit(einheit)
         artikel.set_standardartikel(standardartikel)
-
-    def artikel_löschen(self):
-        pass
-
-    def artikel_speichern(self):
-        pass
-
-    def get_artikel_by_name(self):
-        pass
-
-    def get_artikel_by_id(self):
-        pass
-
-    def get_all_artikel(self):
