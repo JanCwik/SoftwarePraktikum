41ecff604a9e685c0d80dd53b27f77d66c1cb578
SvenjaHolzinger <sh292@hdm-stuttgart.de>
2020-06-03 17:05:11 +0200

    BO anlegen

---
+++ BusinessLogic/namedBO.py
@@ -0,0 +1,22 @@
+from BusinessLogic import BusinessObject
+
+
+class NamedBO(BusinessObject):
+    def __init__(self):
+        super().__init__()
+        self._erstellungs_zeitpunkt = ""
+        self._name = ""
+
+    def set_erstellungs_zeitraum(self, erstellungs_zeitraum):
+        """Setzen der ID."""
+        self._erstellungs_zeitpunkt = erstellungs_zeitraum
+
+    def get_erstellungs_zeitraum(self):
+        """Auslesen der ID."""
+        return self._erstellungs_zeitpunkt
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
