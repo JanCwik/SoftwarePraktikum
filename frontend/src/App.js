d1b153396bc98aac9c4af28fce2905c5035e04bd
Cem Haliscelik <60141373+CemHaliscelik@users.noreply.github.com>
2020-06-04 21:21:52 +0200

    Aufesetzen des frontends

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state={
-            newItem:"",
-            list:[]
-        }
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <div>
-                    Artikel hinzufügen...
-                    <br/>
-                    <input
-                        type="text"
-                        placeholder= "Artikel hier angeben..."
-                        value={this.state.newItem}
-                        onChange={e => this.updateInput("newItem", e.target.value)}
-                        />
-
-
-                </div>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello World
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
