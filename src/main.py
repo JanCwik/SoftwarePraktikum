82d86f6fe8878602e97cbc19445e5c450b2b860c
Andreas Gerbl <65550853+AndreasGerbl@users.noreply.github.com>
2020-06-16 16:30:13 +0200

    API erste Funktionen (delete, post, get) von Artikeln

--- src/main.py
+++ src/main.py
@@ -21,19 +21,19 @@ api = Api(app, version='1.0', title='ShoppingList API',
     description='Das ist unserer API für die Shoppinglist.')
 
 shopping = api.namespace('shopping', description='Funktionen der Shoppinglist')
+
 bo = api.model('BusinessObject', {
     'id': fields.Integer(attribute='_id', description='Der Unique Identifier eines Business Object'),
 })
 
 namedBO = api.inherit('namedBO', bo, {
-    'name': fields.String(attribute='_name', description='Name eines Benutzers'),
-    'erstellungs_zeitpunkt': fields.String(attribute='_erstellungs_zeitpunkt', description='E-Mail-Adresse eines Benutzers'),
+    'name': fields.String(attribute='_name', description='Name des namedBO'),
+    'erstellungs_zeitpunkt': fields.DateTime(attribute='_erstellungs_zeitpunkt', description='Erstellungszeitpunkt'),
 })
 
-"""Users, Customers, Accounts & Transactions sind BusinessObjects..."""
 artikel = api.inherit('Artikel',namedBO , {
-    'Einheit': fields.String(attribute='_einheit', description='Name eines Benutzers'),
-    'Standardartikel': fields.String(attribute='_standardartikel', description='E-Mail-Adresse eines Benutzers'),
+    'Einheit': fields.String(attribute='_einheit', description='Name eines Artikels'),
+    'Standardartikel': fields.String(attribute='_standardartikel', description='Standardartikel'),
 })
 
 
@@ -45,11 +45,46 @@ class ArtikelListOperations(Resource):
     #secured brauchen wir noch nicht
     #@secured
     def get(self):
-        """Auslesen aller Customer-Objekte.
-        Sollten keine Customer-Objekte verfügbar sein, so wird eine leere Sequenz zurückgegeben."""
+        """Auslesen aller Artikel"""
         adm = ApplikationsAdministration()
         artikel = adm.get_all_artikel()
         return artikel
+
+    @shopping.marshal_with(artikel)
+    #@shopping.except(artikel)
+    #@secured
+    def post(self, id):
+        """Anlegen eines Artikels"""
+        adm = ApplikationsAdministration()
+
+        test = Artikel.from_dict(api.payload)
+
+        a = adm.artikel_anlegen(test.get_name(), test.get_einheit(), test.get_standardartikel())
+        return a
+
+
+@shopping.route('/artikel/<int:id>')
+@shopping.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+@shopping.param('id', 'ID des Artikels')
+class ArtikelOperations(Resource):
+    @shopping.marshal_with(artikel)
+    #@secured
+    def get_by_id(self, id):
+        """Auslesen eines bestimmten Artikel"""
+        adm = ApplikationsAdministration()
+        art = adm.get_artikel_by_id(id)
+        return art
+
+    #@secured
+    def delete(self, id):
+        """Löschen eines Artikels anhand einer id"""
+        adm = ApplikationsAdministration()
+        artikel = adm.get_artikel_by_id(id)
+        adm.delete_artikel(artikel)
+        return ''
+
+
+
 
 
 
