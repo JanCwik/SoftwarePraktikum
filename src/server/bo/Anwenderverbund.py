aa92c2b60e48961bb52fc4777b1d71d24e9de46e
SvenjaHolzinger <sh292@hdm-stuttgart.de>
2020-06-23 14:52:18 +0200

    Methoden in der Klasse Anwenderverbund angelegt

--- src/server/bo/Anwenderverbund.py
+++ src/server/bo/Anwenderverbund.py
@@ -5,6 +5,22 @@ class Anwenderverbund(NamedBO):
     def __init__(self):
         super().__init__()
 
+    def benutzer_hinzufügen(self, Benutzer):
+        """Dem Anwenderverbund einen Benutzer hinzufügen"""
+        pass
+
+    def benutzer_löschen(self):
+        """Einen Benutzer aus dem Anwenderverbund löschen"""
+        pass
+
+    def einkaufliste_erstellen(self):
+        """Einen Einkaufsliste erstellen"""
+        pass
+
+    def einkaufsliste_löschen(self):
+        """Einkaufsliste löschen"""
+        pass
+
     def __str__(self):
         """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz."""
         return "Anwenderverbund: {}, {}, {}".format(self.get_id(), self.get_name(), self.get_erstellungs_zeitpunkt())
