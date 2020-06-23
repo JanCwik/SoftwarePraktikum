329c595329122ac2e0aa216c32168c50d0261a96
JanCwik <jc024@hdm-stuttgart.de>
2020-06-23 18:08:29 +0200

    test

--- src/server/ApplikationsAdministration.py
+++ src/server/ApplikationsAdministration.py
@@ -3,7 +3,7 @@ from src.server.bo.Einzelhaendler import Einzelhaendler
 from src.server.db.ArtikelMapper import ArtikelMapper
 from src.server.db.EinzelhaendlerMapper import EinzelhaendlerMapper
 
-
+//2ws
 
 class ApplikationsAdministration(object):
 
