82b9921e30b24467bdb7ae210163a72c6830f6d1
Andreas Gerbl <65550853+AndreasGerbl@users.noreply.github.com>
2020-06-23 17:38:45 +0200

    Einzelhändler alle Methoden Artikel post funktioniert einwandfrei Put Methoden weiterhin fehlerhaft (Key-Error bei Einzelhändler id; Attribute Error bei Artikel)

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from flask_restx import Resource, Api, fields
 
 from src.server.ApplikationsAdministration import ApplikationsAdministration
 from src.server.bo.Artikel import Artikel
+from src.server.bo.Einzelhaendler import Einzelhaendler
 
 
 
@@ -33,9 +34,10 @@ namedBO = api.inherit('namedBO', bo, {
 
 artikel = api.inherit('Artikel',namedBO , {
     'einheit': fields.String(attribute='_einheit', description='Name eines Artikels'),
-    'standardartikel': fields.String(attribute='_standardartikel', description='Standardartikel'),
+    'standardartikel': fields.Integer(attribute='_standardartikel', description='Standardartikel'),
 })
 
+einzelhaendler = api.inherit('Einzelhandler', namedBO)
 
 
 @shopping.route('/artikel')
@@ -52,7 +54,7 @@ class ArtikelListOperations(Resource):
     @shopping.marshal_with(artikel)
     @shopping.expect(artikel)
     #@secured
-    def post(self, id):
+    def post(self):
         """Anlegen eines Artikels"""
         adm = ApplikationsAdministration()
 
@@ -71,8 +73,8 @@ class ArtikelListOperations(Resource):
 class ArtikelOperations(Resource):
     @shopping.marshal_with(artikel)
     #@secured
-    def get_by_id(self, id):
-        """Auslesen eines bestimmten Artikel"""
+    def get(self, id):
+        """Auslesen eines bestimmten Artikel anhand einer id"""
         adm = ApplikationsAdministration()
         artikel = adm.get_artikel_by_id(id)
         return artikel
@@ -89,6 +91,7 @@ class ArtikelOperations(Resource):
     @shopping.expect(artikel)
     #@secured
     def put(self, id):
+        """Update eines durch eine id bestimmten Artikel"""
      
         adm = ApplikationsAdministration()
         a = Artikel.from_dict(api.payload)
@@ -101,16 +104,96 @@ class ArtikelOperations(Resource):
             return '', 500
 
 @shopping.route('/artikel-by-name/<string:name>')
-@shopping.response(500, "Serverfehler")
+@shopping.response(500, 'Serverfehler')
 @shopping.param('name', 'Name des Artikels')
 class ArtikelByNameOperations(Resource):
     @shopping.marshal_with(artikel)
     #@secured
-    def get_by_name(self,name):
-        
+    def get(self, name):
+        """Auslesen eines bestimmten Artikel anhand seines Namen"""
         adm = ApplikationsAdministration()
         artikel = adm.get_artikel_by_name(name)
         return artikel
+
+"""Artikel DONE. Einzelhändler NEXT"""
+
+
+@shopping.route('/einzelhaendler')
+@shopping.response(500, 'Serverfehler')
+class EinzelhaendlerListOperations(Resource):
+    @shopping.marshal_list_with(einzelhaendler)
+    #@secured
+    def get(self):
+        """Auslesen aller Einzelhändler"""
+        adm = ApplikationsAdministration()
+        einzelhaendler = adm.get_all_einzelhaendler()
+        return einzelhaendler
+
+    @shopping.marshal_with(einzelhaendler)
+    @shopping.expect(einzelhaendler)
+    #@secured
+    def post(self):
+        """Anlegen eines Einzelhändlers"""
+        adm = ApplikationsAdministration()
+
+        test = Einzelhaendler.from_dict(api.payload)
+        if test is not None:
+            a = adm.einzelhaendler_anlegen(test.get_id())
+            return a, 200
+        else:
+            return '', 500
+
+
+
+@shopping.route('/einzelhaendler-by-id/<int:id>')
+@shopping.response(500, 'Serverfehler')
+@shopping.param('id', 'ID des Einzelhaendler')
+class EinzelhaendlerOperations(Resource):
+    @shopping.marshal_with(einzelhaendler)
+    #@secured
+    def get(self, id):
+        """Auslesen eines bestimmten Einzelhändlers anhand einer id"""
+        adm = ApplikationsAdministration()
+        einzelhaendler = adm.get_einzelhaendler_by_id(id)
+        return einzelhaendler
+
+    #@secured
+    def delete(self, id):
+        """Löschen eines Einzelhändlers anhand einer id"""
+        adm = ApplikationsAdministration()
+        einzelhaendler = adm.get_einzelhaendler_by_id(id)
+        adm.delete_einzelhaendler(einzelhaendler)
+        return ''
+
+    @shopping.marshal_with(einzelhaendler)
+    @shopping.expect(einzelhaendler)
+    #@secured
+    def put(self, id):
+        """Update eines durch eine id bestimmten Einzelhändlers"""
+
+        adm = ApplikationsAdministration()
+        a = Einzelhaendler.from_dict(api.payload)
+
+        if a is not None:
+            a.set_id(id)
+            adm.update_einzelhaendler(einzelhaendler)
+            return '', 200
+        else:
+            return '', 500
+
+@shopping.route('/einzelhaendler-by-name/<string:name>')
+@shopping.response(500, 'Serverfehler')
+@shopping.param('name', 'Name des Einzelhändler')
+class ArtikelByNameOperations(Resource):
+    @shopping.marshal_with(einzelhaendler)
+    #@secured
+    def get(self, name):
+        """Auslesen eines bestimmten Einzelhändlers anhand seines Namen"""
+        adm = ApplikationsAdministration()
+        einzelhaendler = adm.get_einzelhaendler_by_name(name)
+        return einzelhaendler
+
+
 
 
 
