6933f6cdb7d82bde47af1d65b787a2c8bf5bbfd1
TimmLouis <timmlouis@googlemail.com>
2020-06-24 23:45:31 +0200

    API und APP leicht umgeschrieben -> damit es etwas leichter verständlich aufgebaut ist

--- frontend/src/api/API.js
+++ frontend/src/api/API.js
@@ -1,4 +1,4 @@
-
+import ArtikelBO from "./ArtikelBO";
 
 
 export default class API {
@@ -6,7 +6,7 @@ export default class API {
     // Diese Klasse wird zurzeit nicht verwendet. Auf diese Weise wurde die API im Bankbeispiel umgesetzt,
     // aber diese Umsetzteung funktioniert hier noch nicht
 
-    static #api = null;
+     static #api = null;
 
 
     //Attribut um im späteren verlauf des Projekts das angeben aller URLs zu erleichtern
@@ -17,37 +17,23 @@ export default class API {
 
 
     //diese Klassenmethode realisiert die Umsetztung als Singelton, dadurch kann nur eine Instanz dieser Klasse existieren
-    static getAPI() {
-        if (this.#api == null) {
-            this.#api = new API();
-        }
-        return this.#api;
-    }
+     static getAPI() {
+          if (this.#api == null) {
+              this.#api = new API();
+          }
+          return this.#api;
+      }
 
-
-
-123
-
-    //führt die fetch-Funktion aus, fängt dabei mögliche Errors ab und führt anschließend schon die json-Funktion mit der Response aus.
-    #fetchAdvanced = (url, init) => fetch(url, init)
-        .then(res => {
-
-                if (!res.ok) {
-                    throw Error(`${res.status} ${res.statusText}`);
-                }
-                return res.json();
-            }
-        )
 
 
 
 
     // Klassenmethode die den GET request ausführt und alle in der Datenbank gespeicherten Artikel ausgibt
     static getArtikel() {
-        return this.#fetchAdvanced(this.#getArtikelURL()).then((responseJSON) => {      // funktioniert wahrscheinlich wendern nur mit der URL
-                                                                                         // http://127.0.0.1:5000/shopping/artikel
-            let artikel = responseJSON;
-          
+        return this.#fetchAdvanced("http://127.0.0.1:5000/shopping/artikel").then((responseJSON) => {      // funktioniert wahrscheinlich wendern nur mit der URL
+                                                                                                                // http://127.0.0.1:5000/shopping/artikel
+            let artikel = ArtikelBO.fromJSON(responseJSON);
+
 
             return new Promise(function (resolve) {
                 resolve(artikel);
@@ -55,5 +41,18 @@ export default class API {
             })
         })
     }
-}
 
+
+   //führt die fetch-Funktion aus, fängt dabei mögliche Errors ab und führt anschließend schon die json-Funktion mit der Response aus.
+    #fetchAdvanced = (url, init) =>
+        fetch(url, init)
+            .then(res => {
+
+                    if (!res.ok) {
+                        throw Error(`${res.status} ${res.statusText}`);
+                    }
+                    return res.json();
+                }
+            )
+
+}
