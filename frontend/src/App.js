cd96617ed2784750d2a54a913ed77f36ad2d0570
TimmLouis <timmlouis@googlemail.com>
2020-06-24 23:45:31 +0200

    API und APP leicht umgeschrieben -> damit es etwas leichter verständlich aufgebaut ist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Anwenderverbund from "./components/Anwenderverbund";
 import Einzelhaendler from "./components/Einzelhaendler";
 import { Grid, Typography } from '@material-ui/core';
 import AnwenderverbundBO from "./api/AnwenderverbundBO";
+import ArtikelBO from "./api/ArtikelBO";
 
 //import {Route} from 'react-router-dom'
 
@@ -21,31 +22,50 @@ class App extends React.Component {
         }
     }
 
+     // wenn der Component "durchlaufen" wurde wird die methode getArtikelAPP aufgerufen
+    componentDidMount() {
+        this.getArtikelAPP();
+        }
 
 
-
-/*
      //Auf diese weise wurde im bank beispiel auf die API zugegriffen aber diese Umsetzteung funktioniert hier noch nicht
 
 
      // Ruft die Methode getArtikel der Klasse API auf und speichert die Response des GET Requests in einem Array "artikel" im State
      // Bei einem Error wird nichts in den State geschrieben
      getArtikelAPP = () => {
-        API.getAPI().getArtikel()
+        //API.getAPI()
+        this
+            .getArtikel()
             .then(artikel =>
                 this.setState({
           artikel: artikel
 
         })).catch(e =>
           this.setState({
+              artikel: []
           })
         );
        }
-    // wenn der Component "durchlaufen" wurde wird die methode getArtikelAPP aufgerufen
-    componentDidMount() {
-        this.getArtikelAPP();
-        }
-*/
+
+
+
+
+    //diese zwei funktionen stehen genau so nochmal in der API Klasse
+
+    //methode die den GET request ausführt und alle in der Datenbank gespeicherten Artikel ausgibt
+    getArtikel() {
+        return this.#fetchAdvanced("http://127.0.0.1:5000/shopping/artikel").then((responseJSON) => {
+
+            let artikel = ArtikelBO.fromJSON(responseJSON);
+
+
+            return new Promise(function (resolve) {
+                resolve(artikel);
+
+            })
+        })
+    }
 
   //führt die fetch-Funktion aus, fängt dabei mögliche Errors ab und führt anschließend schon die json-Funktion mit der Response aus.
   #fetchAdvanced = (url, init) => fetch(url, init)
@@ -57,7 +77,8 @@ class App extends React.Component {
             return res.json();
         }
     )
-
+/*
+    // vereinfachte umsetztung. funktioniert aber ist unsauber
     //führt einen GET Request aus und fetcht die Response (alle Artikel aus der Datenbank), schreibt sie in die Console
     // und speichert sie in einem Array "artikel" im State
     componentDidMount() {
@@ -70,7 +91,7 @@ class App extends React.Component {
             })
         }
 
-
+*/
 
     render() {
         const {artikel} = this.state
