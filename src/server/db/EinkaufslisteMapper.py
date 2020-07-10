f03c17e4142bad714aacfa29ba1b470156ff450d
SvenjaHolzinger <sh292@hdm-stuttgart.de>
2020-07-10 13:49:10 +0200

    Kommentare zu EinkaufslisteMapper vervollständigt

--- src/server/db/EinkaufslisteMapper.py
+++ src/server/db/EinkaufslisteMapper.py
@@ -10,9 +10,10 @@ class EinkaufslistenMapper(Mapper):
     def __init__(self):
         super().__init__()
 
-    """ Mapper-Methode zum speichern einer neuen Einkaufsliste in der Datenbank"""
+    def insert(self, einkaufsliste):
+        """ Mapper-Methode zum speichern einer neuen Einkaufsliste in der Datenbank.
 
-    """ Beim Aufruf der Methode wird eine zuvor erstellte Instanz der Klasse "Einkaufsliste()" übergeben.
+        Beim Aufruf der Methode wird eine zuvor erstellte Instanz der Klasse "Einkaufsliste()" übergeben.
         Anschließend wird via SQL-Abfrage die höchste ID aus der Tabelle "einkaufsliste" ausgegeben und dann
         mit der fetchall-Methode in einem Tupel gespeichert.
 
@@ -22,11 +23,8 @@ class EinkaufslistenMapper(Mapper):
         Falls noch keine ID in der Tabelle vorhanden sein sollte, wird die Zahl 1 an die Instanz weitergegeben
 
         Dann erfolgt erneut ein SQL-Statement welches die Instanz in der Datenbank speichert.
-        Mittels der getter-Methoden, welche zuvor in der entsprechenden Business-Object-Klasse definierten wurden, 
-        werden die Attribute der Instanz an das SQL-Statement übergeben.
-        """
-    def insert(self, einkaufsliste):
-
+        Mittels der getter-Methoden, welche zuvor in der entsprechenden Business-Object-Klasse definierten wurden,
+        werden die Attribute der Instanz an das SQL-Statement übergeben."""
         id_standard = True
         neueID = None
 
@@ -46,49 +44,38 @@ class EinkaufslistenMapper(Mapper):
         vals = (einkaufsliste.get_id(), einkaufsliste.get_name(), einkaufsliste.get_erstellungs_zeitpunkt(), einkaufsliste.get_änderungs_zeitpunkt(), einkaufsliste.get_anwenderId())
         cursor.execute(template, vals)
 
-
-
-
         command = "SELECT id FROM artikel WHERE standardartikel={}".format(id_standard)
         cursor.execute(command)
         tuples = cursor.fetchall()
 
-
         for i in tuples:
             for i in i:
-
-
                 cursor.execute("SELECT MAX(id) AS maxid_listeneintrag FROM listeneintrag ")
                 ins = cursor.fetchall()
 
                 for (maxid_listeneintrag) in ins:
                     if maxid_listeneintrag[0] is not None:
-
                         neueID = (maxid_listeneintrag[0] + 1)
                     else:
-
                         neueID = 1
 
                 template2 = "INSERT INTO listeneintrag (id, einkaufsliste_id, artikel_id, erledigt) VALUES (%s,%s,%s,%s)"
                 vals2 = (neueID, einkaufsliste.get_id(), i, False)
                 cursor.execute(template2, vals2)
 
-
-
         self._cnx.commit()
         cursor.close()
 
         return einkaufsliste
 
-    """ Mapper-Methode zum aktualisieren (des Namens) einer Einkaufsliste in der Datenbank"""
+    def update(self, einkaufsliste):
+        """ Mapper-Methode zum aktualisieren (des Namens) einer Einkaufsliste in der Datenbank.
 
-    """ Beim Aufruf Methode wird eine zuvor erstellte Instanz der Klasse "Einkaufsliste()" übergeben.
+        Beim Aufruf Methode wird eine zuvor erstellte Instanz der Klasse "Einkaufsliste()" übergeben.
         Zuerst wird der Zeitpunkt festgehalten, wann das Update erfolgt ist.
         Dann erfolgt ein SQL-Statement welches das Objekt in der Datenbank aktualisiert.
-        Mittels der getter-Methoden, welche zuvor in der entsprechenden Business-Object-Klasse definierten wurden, 
+        Mittels der getter-Methoden, welche zuvor in der entsprechenden Business-Object-Klasse definierten wurden,
         werden die Attribute der Instanz an das SQL-Statement übergeben."""
-
-    def update(self, einkaufsliste):
         aenderungs_zeitpunkt = datetime.datetime.now()
 
         cursor = self._cnx.cursor()
@@ -100,20 +87,15 @@ class EinkaufslistenMapper(Mapper):
         self._cnx.commit()
         cursor.close()
 
-    """ Mapper-Methode zum ausgeben einer Einkaufslsite anhand deren ID"""
+    def find_by_id(self, id):
+        """ Mapper-Methode zum ausgeben einer Einkaufslsite anhand deren ID.
 
-    """ Beim Aufruf der Methode wird eine ID in der Variablen "id" gespeichert, welche schließlich an das SQL-Statement übergeben wird.
+        Beim Aufruf der Methode wird eine ID in der Variablen "id" gespeichert, welche schließlich an das SQL-Statement übergeben wird.
         Das entsprechende Objekt, welches aus der Datenbank ausgegeben wird, wird in einem Tupel gespeichert.
         Anschließend werden die einzelnen Attribute aus dem Tupel an der Stelle 0 genommen und an eine neue Artikel-Instanz via
         den Setter-Methoden übergeben.
         Sollte die Datenbank anhand der ID kein Objekt zurückliefern, wird ausgegeben was innerhalb des IndexErrors steht --> None
         Das Ergebnis wir schließlich von der Mehtode zurückgegeben."""
-
-    def find_by_id(self, id):
-
-
-        result = None
-
         cursor = self._cnx.cursor()
         command = "SELECT id, name, erstellungs_zeitpunkt, aenderungs_zeitpunkt, anwenderverbund_id FROM einkaufsliste WHERE id={}".format(id)
         cursor.execute(command)
@@ -129,8 +111,6 @@ class EinkaufslistenMapper(Mapper):
             einkaufsliste.set_anwenderId(anwenderverbund_id)
             result = einkaufsliste
         except IndexError:
-            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
-            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
             result = None
 
         self._cnx.commit()
@@ -138,19 +118,15 @@ class EinkaufslistenMapper(Mapper):
 
         return result
 
-    """ Mapper-Methode zum ausgeben einer Einkaufsliste anhand deren Name"""
+    def find_by_name(self, name):
+        """ Mapper-Methode zum ausgeben einer Einkaufsliste anhand deren Name.
 
-    """ Beim Aufruf der Methode wird ein Name in der Variablen "name" gespeichert, welche schließlich an das SQL-Statement übergeben wird.
+        Beim Aufruf der Methode wird ein Name in der Variablen "name" gespeichert, welche schließlich an das SQL-Statement übergeben wird.
         Das entsprechende Objekt, welches aus der Datenbank ausgegeben wird, wird in einem Tupel gespeichert.
         Anschließend werden die einzelnen Attribute aus dem Tupel an der Stelle 0 genommen und an eine neue Artikel-Instanz via
         den Setter-Methoden übergeben.
         Sollte die Datenbank anhand des Namens kein Objekt zurückliefern, wird ausgegeben was innerhalb des IndexErrors steht --> None
-        Das Ergebnis wir schließlich von der Mehtode zurückgegeben.
-        """
-
-    def find_by_name(self, name):
-
-        result = None
+        Das Ergebnis wir schließlich von der Mehtode zurückgegeben."""
 
         cursor = self._cnx.cursor()
         command = "SELECT id, name, erstellungs_zeitpunkt, aenderungs_zeitpunkt, anwenderverbund_id FROM einkaufsliste WHERE name LIKE '{}' ORDER BY name".format(name)
@@ -167,8 +143,6 @@ class EinkaufslistenMapper(Mapper):
             einkaufsliste.set_anwenderId(anwenderverbund_id)
             result = einkaufsliste
         except IndexError:
-            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
-            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
             result = None
 
         self._cnx.commit()
@@ -176,15 +150,13 @@ class EinkaufslistenMapper(Mapper):
 
         return result
 
-    """ Mapper-Methode zum löschen einer Einkaufsliste aus der Datenbank"""
-
-    """ Beim Aufruf Methode wird eine zuvor erstellte Instanz der Klasse "Einkaufsliste()" übergeben.
-        Dann erfolgt ein SQL-Statement welches das Objekt aus der Datenbank löscht.
-        Mittels der getter-Methode, welche zuvor in der entsprechenden Business-Object-Klasse definierten wurde, 
-        wird die entsprechende ID der Instanz an das SQL-Statement übergeben.."""
-
     def delete(self, einkaufsliste):
+        """ Mapper-Methode zum löschen einer Einkaufsliste aus der Datenbank.
 
+        Beim Aufruf Methode wird eine zuvor erstellte Instanz der Klasse "Einkaufsliste()" übergeben.
+        Dann erfolgt ein SQL-Statement welches das Objekt aus der Datenbank löscht.
+        Mittels der getter-Methode, welche zuvor in der entsprechenden Business-Object-Klasse definierten wurde,
+        wird die entsprechende ID der Instanz an das SQL-Statement übergeben."""
         cursor = self._cnx.cursor()
 
         eintraege = "DELETE FROM listeneintrag WHERE einkaufsliste_id={}".format(einkaufsliste.get_id())
@@ -197,6 +169,7 @@ class EinkaufslistenMapper(Mapper):
         cursor.close()
 
     def find_all_listeneintraege(self, einkaufsliste):
+        """Mapper-Methode zum ausgeben aller Listeneinträge zu einer EInkaufsliste."""
         id = einkaufsliste.get_id()         #erst hier wird die id der Einkaufsliste ausgelesen
         result = []
         cursor = self._cnx.cursor()
@@ -204,7 +177,6 @@ class EinkaufslistenMapper(Mapper):
         res = cursor.fetchall()
 
         for(id, anzahl, aenderungs_zeitpunkt, einkaufsliste_id, einzelhaendler_id, artikel_id, benutzer_id, erledigt) in res:
-
             listeneintrag = Listeneintrag()
             listeneintrag.set_id(id)
             listeneintrag.set_anzahl(anzahl)
