bbbe1073073f2ca563143815d46759f1903b7db2
SvenjaHolzinger <sh2929@hdm-stuttgart.de>
2020-07-14 13:59:33 +0200

    Die Methode def get_top_Einzelhaendler(self) erstellt

--- src/server/db/StatistikMapper.py
+++ src/server/db/StatistikMapper.py
@@ -41,3 +41,33 @@ class StatistikMapper(Mapper):
         cursor.close()
         return top
 
+    def get_top_Einzelhaendler(self):
+        EinzelhaendlerIDsInt = []
+        top = []
+
+        cursor = self._cnx.cursor()
+
+        command = "SELECT einzelhaendler_id FROM listeneintrag"
+        cursor.execute(command)
+        EinzelhaendlerIDs = cursor.fetchall()
+
+        for i in EinzelhaendlerIDs:
+            for i in i:
+                EinzelhaendlerIDsInt.append(i)
+
+        EinzelhaendlerIDsInt = list(set(EinzelhaendlerIDsInt))
+
+        for k in EinzelhaendlerIDsInt:
+            cursor.execute("SELECT SUM(anzahl) FROM listeneintrag where einzelhaendler_id={}".format(k))
+            gesamtZahl = cursor.fetchall()
+
+            for i in gesamtZahl:
+                for i in i:
+                    statistik = Statistik()
+                    statistik.set_EinzelhaendlerID(k)
+                    statistik.set_gesamtzahl(i)
+                    top.append(statistik)
+
+        self._cnx.commit()
+        cursor.close()
+        return top
