736524dbe58421b649262c26b4b600c5a5280cf7
SvenjaHolzinger <sh2929@hdm-stuttgart.de>
2020-07-15 13:45:25 +0200

    Die Statistik bo get und set Methoden

--- src/server/bo/StatistikHändler.py
+++ src/server/bo/StatistikHändler.py
@@ -1,7 +1,16 @@
 from src.server.bo.Statistik import Statistik
 
+
 class StatistikHaendler(Statistik):
 
     def __init__(self):
         super().__init__()
         self._Einzelhaendler_name = ""
+
+    def set_Einzelhaendler_name(self, Einzelhaendler_name):
+        """Setzen des Namens eines Einzelhaendlers"""
+        self._Einzelhaendler_name = Einzelhaendler_name
+
+    def get_Einzelhaendler_name(self):
+        """Auslesen des Namens eines Einzelhaendlers"""
+        return self._Einzelhaendler_name
--- src/server/bo/StatistikZeitraum.py
+++ src/server/bo/StatistikZeitraum.py
@@ -4,5 +4,21 @@ class StatistikZeitraum(Statistik):
 
     def __init__(self):
         super().__init__()
-        self._von=""
-        self._bis=""
+        self._von = ""
+        self._bis = ""
+
+    def set_von(self, von):
+        """Setzen des Beginnzeitpunkts"""
+        self._von = von
+
+    def get_von(self):
+        """Auslesen des Beginnzeitpunkts"""
+        return self._von
+
+    def set_bis(self, bis):
+        """Setzen des Beginnzeitpunkts"""
+        self._bis = bis
+
+    def get_bis(self):
+        """Auslesen des Beginnzeitpunkts"""
+        return self._bis
