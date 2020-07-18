84eb309dbe90a36d266ac70024275e7f56e20a72
TimmLouis <timmlouis@googlemail.com>
2020-07-18 21:27:07 +0200

    ListeneintragForm.js verbessert und kommentiert

--- frontend/src/components/Einkaufsliste.js
+++ frontend/src/components/Einkaufsliste.js
@@ -140,7 +140,7 @@ class Einkaufsliste extends Component {
 
           listeneintraege.map(listeneintrag =>
             <ListenEintrag key={listeneintrag.getID()} listeneintrag={listeneintrag} einkaufsliste={this.props.location.einkaufsliste} reload={this.reload}
-              onListeneintragDeleted={this.listeneintragDeleted} reload={this.reload}
+              onListeneintragDeleted={this.listeneintragDeleted}
             />)
         }
         <LoadingProgress show={loadingInProgress} />
