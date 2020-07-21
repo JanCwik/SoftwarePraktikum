7b78557a808a94b029b0f5f4aafe658491660996
SvenjaHolzinger <sh292@hdm-stuttgart.de>
2020-07-21 18:58:35 +0200

    Statistikmapper in ListeneintragMapper

--- src/server/db/ListeneintragMapper.py
+++ src/server/db/ListeneintragMapper.py
@@ -1,5 +1,7 @@
 from src.server.db.Mapper import Mapper
 from src.server.bo.Listeneintrag import Listeneintrag
+from src.server.bo.Statistik import Statistik
+from src.server.bo.StatistikZeitraum import StatistikZeitraum
 import datetime
 
 
@@ -239,3 +241,67 @@ class ListeneintragMapper(Mapper):
 
         self._cnx.commit()
         cursor.close()
+
+    def get_all_listeneintraege_by_benutzer(self, benutzer):
+        cursor = self._cnx.cursor()
+
+        template = "SELECT artikel_id FROM listeneintrag " + "WHERE benutzer_id=%s AND erledigt=%s"
+        vals = (benutzer.get_id(), 1)
+        cursor.execute(template, vals)
+        artikel = cursor.fetchall()
+
+        self._cnx.commit()
+        cursor.close()
+        return artikel
+
+    def get_all_listeneintraege_by_Einzelhaendler(self, benutzer, einzelhaendler):
+        cursor = self._cnx.cursor()
+
+        template = "SELECT artikel_id FROM listeneintrag " + "WHERE einzelhaendler_id=%s AND benutzer_id=%s AND erledigt=%s"
+        vals = (einzelhaendler.get_id(), benutzer.get_id(), 1)
+        cursor.execute(template, vals)
+        artikel = cursor.fetchall()
+
+        self._cnx.commit()
+        cursor.close()
+        return artikel
+
+    def get_all_listeneintraege_by_Datum(self, benutzer):
+        eintraege = []
+        cursor = self._cnx.cursor()
+
+        template = "SELECT artikel_id, aenderungs_zeitpunkt FROM listeneintrag " + "WHERE benutzer_id=%s AND erledigt=%s"
+
+        vals = (benutzer.get_id(), 1)
+        cursor.execute(template, vals)
+        artikel = cursor.fetchall()
+
+        for (artikel_id, aenderungs_zeitpunkt) in artikel:
+            instanz = StatistikZeitraum()
+            instanz.set_ArtikelID(artikel_id)
+            instanz.set_zeitpunkt(aenderungs_zeitpunkt)
+            eintraege.append(instanz)
+
+        self._cnx.commit()
+        cursor.close()
+        return eintraege
+
+    def get_all_listeneintraege_by_Einzelhaendler_Datum(self, benutzer, einzelhaendler):
+        eintraege = []
+        cursor = self._cnx.cursor()
+
+        template = "SELECT artikel_id, aenderungs_zeitpunkt FROM listeneintrag " + "WHERE einzelhaendler_id=%s AND benutzer_id=%s AND erledigt=%s"
+
+        vals = (einzelhaendler.get_id(), benutzer.get_id(), 1)
+        cursor.execute(template, vals)
+        artikel = cursor.fetchall()
+
+        for (artikel_id, aenderungs_zeitpunkt) in artikel:
+            instanz = StatistikZeitraum()
+            instanz.set_ArtikelID(artikel_id)
+            instanz.set_zeitpunkt(aenderungs_zeitpunkt)
+            eintraege.append(instanz)
+
+        self._cnx.commit()
+        cursor.close()
+        return eintraege
